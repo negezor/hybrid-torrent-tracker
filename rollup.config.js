eb7fd0e52a9b486ca34566a32fec5da80cc8dfc5
Negezor <negezor@gmail.com>
2018-09-03 17:33:26 +1100

    feat: Usage rollup for build

---
+++ rollup.config.js
@@ -0,0 +1,48 @@
+import babel from 'rollup-plugin-babel';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+
+import pkg from './package.json';
+
+const babelConfig = require('./babel.config');
+
+const dependencies = Object.keys(pkg.dependencies);
+
+export default [
+	{
+		input: 'src/index.mjs',
+		external: [
+			...dependencies,
+			'crypto',
+			'stream',
+			'https',
+			'http',
+			'util',
+			'url',
+			'fs'
+		],
+		plugins: [
+			resolve({
+				extensions: ['.mjs', '.js'],
+				preferBuiltins: true,
+			}),
+			babel({
+				...babelConfig,
+
+				babelrc: false
+			}),
+			commonjs()
+		],
+		output: [
+			{
+				file: `${pkg.main}.js`,
+				format: 'cjs',
+				exports: 'named'
+			},
+			{
+				file: `${pkg.main}.mjs`,
+				format: 'esm'
+			}
+		]
+	}
+];
